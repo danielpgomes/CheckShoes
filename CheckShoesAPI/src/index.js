500ea152e64aa2040415144df5eb2b7508faaa2c
lucafHEG <luca.furlan@hes-so.ch>
2023-10-13 17:17:23 -0700

    show all events

--- CheckShoesAPI/src/index.js
+++ CheckShoesAPI/src/index.js
@@ -1,47 +1,46 @@
+const e = require('express')
 const express = require('express')
 const app = express()
 const port = 9001
 // module FileSystem pour lire, écrire des fichiers...
-const fs = require('fs')
+const fs = require('fs').promises
 // module pour travailler les chemins de fichier
 const path = require('path')
 
 // chemin à changer en respectant le standard EPCIS 2.0
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   
   // attention le repertoire ou est stocké les fichiers sont pas les mêmes chez vous !!!
   const diretoryPath = 'C:/Users/Luca/Desktop/CheckShoes/eventfiles';
   
-  // méthode pour lire les directory
-  fs.readdir(diretoryPath, (error, files) => {
-    if (error) {
-      res.status(500).send('erreur avec le répertoire' + error)
-    } else {
-      // stocker les events
-      const events = [];
+  try {
+    // récupère les fichiers du directory
+    const files = await fs.readdir(diretoryPath);
 
-      files.forEach((file) => {
-        // joindre tous les fichiers du repertoire
-        const filepath = path.join(diretoryPath, file);
+    const events = [];
 
-        // block try and catch pour voir ou est le problème 
-        try{
-          // lire les fichiers en utf-8 
-          const data = fs.readFileSync(filepath, 'utf8');
-          // parser les fichiers txt en JSON
-          const event = JSON.parse(data);
-          // ajouter un event en json object dans la liste events
-          events.push(event);
-          // gérer les erreurs 
-        } catch (readError) {
-          console.error('erreur avec les datas' + readError);
-        }
-      })
-      // retourer le résultats en HTTP 
-      res.json(events);
+    // parcourir les fichiers
+    for (const file of files){
+
+      // gère déjà les chemins de fichier en parcourant le directory
+      const filePath = path.join(diretoryPath, file);
+      try{
+        // lire le fichier en fichier txt
+        const data = await fs.readFile(filePath, 'utf8');
+        // serialize le fichier txt 
+        const event = JSON.parse(data);
+        // ajoute un event
+        events.push(event);
+      } catch (readError) {
+        console.error('Error reading files', readError);
+      }
     }
-  })
-  
+    // la liste d'event en json dans le HTTP  
+    res.json(events);
+
+  } catch(error) {
+    res.status(500).send('Error reading directory' + error);
+  }
 })
 
 app.listen(port, () => {
