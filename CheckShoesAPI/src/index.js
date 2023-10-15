eaca27ecfcb428abc8b17af8ea7a3ce755c11ef0
Daniel <daniel.pereira@etu.hesge.ch>
2023-10-15 14:17:12 +0200

    recherche par code chaussure

--- CheckShoesAPI/src/index.js
+++ CheckShoesAPI/src/index.js
@@ -9,10 +9,9 @@ const path = require('path')
 
 // chemin à changer en respectant le standard EPCIS 2.0
 app.get('/', async (req, res) => {
-  
+
   // attention le repertoire ou est stocké les fichiers sont pas les mêmes chez vous !!!
-  const diretoryPath = 'C:/Users/Luca/Desktop/CheckShoes/eventfiles';
-  
+  const diretoryPath = 'C:/CheckShoesv1/CheckShoes/eventfiles';
   try {
     // récupère les fichiers du directory
     const files = await fs.readdir(diretoryPath);
@@ -29,8 +28,19 @@ app.get('/', async (req, res) => {
         const data = await fs.readFile(filePath, 'utf8');
         // serialize le fichier txt 
         const event = JSON.parse(data);
+
+        const numeroRecherche = '01200009000011';
+        console.log("Voici le code chaussure : " + numeroRecherche);
+
+        for (const epc of event.epcisBody.eventList[0].epcList) {
+            if (epc.includes(numeroRecherche)) {
+              console.log('Fichier correspondant trouvé :', file);
+              console.log('Contenu du fichier :', event);
+              events.push(event);
+            }
+          }
         // ajoute un event
-        events.push(event);
+        //events.push(event);
       } catch (readError) {
         console.error('Error reading files', readError);
       }
